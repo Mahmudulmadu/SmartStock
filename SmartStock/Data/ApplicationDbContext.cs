ba9eabb587711eaa4a679a90f147b091e39bfd86
Madu <mahmudulmadu@gmail.com>
2025-06-18 01:11:28 +0600

    Add project files.

---
+++ SmartStock/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartStock.Models;
+using System.Collections.Generic;
+
+namespace SmartStock.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Smartphone", Category = "Electronics", Price = 25000, Quantity = 15 },
+                new Product { Id = 2, Name = "T-Shirt", Category = "Clothing", Price = 600, Quantity = 100 },
+                new Product { Id = 3, Name = "Rice 5kg", Category = "Food", Price = 500, Quantity = 50 },
+                new Product { Id = 4, Name = "Headphones", Category = "Electronics", Price = 1200, Quantity = 40 },
+                new Product { Id = 5, Name = "Jeans", Category = "Clothing", Price = 1200, Quantity = 60 }
+            );
+        }
+    }
+}
---
+++ SmartStock/Migrations/20250617190857_initialDbCreate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SmartStock.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialDbCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Name", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Electronics", "Smartphone", 25000m, 15 },
+                    { 2, "Clothing", "T-Shirt", 600m, 100 },
+                    { 3, "Food", "Rice 5kg", 500m, 50 },
+                    { 4, "Electronics", "Headphones", 1200m, 40 },
+                    { 5, "Clothing", "Jeans", 1200m, 60 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
